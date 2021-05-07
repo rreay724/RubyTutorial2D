57e1e2a51402afc33485e7f53df09898286683b5
Bobby Reay <rreay724@gmail.com>
2021-05-07 16:21:27 -0400

    in progress

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -6,6 +6,11 @@ public class RubyController : MonoBehaviour
 {
 
     public int maxHealth = 5;
+
+    public float timeInvincible = 2.0f;
+    bool isInvincible;
+    float invincibleTimer;
+
     public float characterSpeed = 3.0f;
     public int health { get { return currentHealth; } }
     int currentHealth;
@@ -19,7 +24,6 @@ public class RubyController : MonoBehaviour
     {
         rigidBody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
-        currentHealth = 1;
     }
 
     // Update is called once per frame
@@ -28,7 +32,12 @@ public class RubyController : MonoBehaviour
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
 
-        
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+                isInvincible = false;
+        }
     }
 
     // In addition to this, the physics system is updated at a
@@ -60,6 +69,15 @@ public class RubyController : MonoBehaviour
 
  public void ChangeHealth(int amount)
     {
+        if(amount < 0)
+        {
+            if (isInvincible)
+                return;
+
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
+
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
